52fc3712a688a0183672b5b29b5e258b7ff498b2
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-10 20:52:56 +0100

    fix: added logic to reference either sds ptl or ptlsbox

--- components/routes/interpolated-defaults.tf
+++ components/routes/interpolated-defaults.tf
@@ -1,5 +1,9 @@
 locals {
-  ssptl_sub_id = "6c4d2513-a873-41b4-afdd-b05a33206631"
+  is_sbox                   = length(regexall(".*(s?box).*", var.env)) > 0
+  ssptl_sub_id              = local.is_sbox ? "64b1c6d6-1481-44ad-b620-d8fe26a2c768" : "6c4d2513-a873-41b4-afdd-b05a33206631"
+  ssptl_vnet_name           = local.is_sbox ? "ss-ptlsbox-vnet" : "ss-ptl-vnet"
+  ssptl_route_table_name    = local.is_sbox ? "aks-ptlsbox-route-table" : "aks-ptl-route-table"
+  ssptl_vnet_resource_group = local.is_sbox ? "ss-ptlsbox-network-rg" : "ss-ptl-network-rg"
   landing_zone_prefixes = flatten([
     for landing_zone in var.landing_zones : landing_zone.address_space
   ])
@@ -14,6 +18,6 @@ module "ctags" {
 }
 
 data "azurerm_route_table" "ssptl" {
-  name                = "aks-ptl-route-table"
-  resource_group_name = "ss-ptl-network-rg"
+  name                = local.ssptl_route_table_name
+  resource_group_name = local.ssptl_vnet_resource_group
 }
