c010e6791194561f894ff1c00876f1d273b81b6d
Alex Bance <Alex.Bance@justice.gov.uk>
2023-11-15 11:00:08 +0000

    fix: locals -> local

--- components/landing/role-assignments.tf
+++ components/landing/role-assignments.tf
@@ -1,19 +1,19 @@
 resource "azurerm_role_assignment" "users" {
-  for_each             = setproduct(locals.users_set, local.rbac_scopes)
+  for_each             = setproduct(local.users_set, local.rbac_scopes)
   scope                = each.value[1]
   role_definition_name = "Owner"
   principal_id         = data.azuread_user.principal[each.value[0]].id
 }
 
 resource "azurerm_role_assignment" "groups" {
-  for_each             = setproduct(locals.groups_set, local.rbac_scopes)
+  for_each             = setproduct(local.groups_set, local.rbac_scopes)
   scope                = each.value[1]
   role_definition_name = "Owner"
   principal_id         = data.azuread_user.principal[each.value[0]].id
 }
 
 resource "azurerm_role_assignment" "service_principals" {
-  for_each             = setproduct(locals.service_principals_set, local.rbac_scopes)
+  for_each             = setproduct(local.service_principals_set, local.rbac_scopes)
   scope                = each.value[1]
   role_definition_name = "Owner"
   principal_id         = data.azuread_user.principal[each.value[0]].id
