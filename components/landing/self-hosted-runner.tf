17e008cb4737555b844ecaa3493dc9b8da1d5d72
Alex Bance <Alex.Bance@justice.gov.uk>
2024-02-05 16:51:24 +0000

    fix: update rg name

--- components/landing/self-hosted-runner.tf
+++ components/landing/self-hosted-runner.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_group" "runner" {
   for_each            = var.landing_zones
   name                = "ingest${each.key}-runner-${var.env}"
   location            = "uksouth"
-  resource_group_name = module.data_landing_zone[each.key].resource_groups["management"].name
+  resource_group_name = "ingest${each.key}-management-${var.env}"
   os_type             = "Linux"
   ip_address_type     = "Private"
   subnet_ids          = [module.data_landing_zone[each.key].subnet_ids["vnet-services"]]
