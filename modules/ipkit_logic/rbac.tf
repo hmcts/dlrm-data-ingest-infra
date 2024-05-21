ce3a78f2c786c774b63428fe3f0353b20f91012f
Alex Bance <Alex.Bance@justice.gov.uk>
2024-05-21 19:29:03 +0100

    fix: correct reference to identity

--- modules/ipkit_logic/rbac.tf
+++ modules/ipkit_logic/rbac.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "storage" {
-  principal_id         = azurerm_logic_app_standard.this.identity.principal_id
+  principal_id         = azurerm_logic_app_standard.this.identity[0].principal_id
   scope                = module.storage.storageaccount_id
   role_definition_name = "Storage Blob Data Contributor"
 }
