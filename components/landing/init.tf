e773c3b5450a243f84f60e4ce24b64132f8c4e83
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-09-26 15:39:26 +0100

    importing keyault

--- components/landing/init.tf
+++ components/landing/init.tf
@@ -59,3 +59,8 @@ provider "azurerm" {
   features {}
   subscription_id = var.env == "prod" || var.env == "production" ? "8999dec3-0104-4a27-94ee-6588559729d1" : var.env == "sbox" || var.env == "sandbox" ? "bf308a5c-0624-4334-8ff8-8dca9fd43783" : "1c4f0704-a29e-403d-b719-b90c34ef14c9"
 }
+
+import {
+  to = module.data_landing_zone["00"].module.shared_integration_datafactory.azurerm_data_factory_managed_private_endpoint.this["keyvault"]
+  id = "/subscriptions/e9674938-57cb-43ff-a440-1917658e468c/resourceGroups/ingest00-main-prod/providers/Microsoft.DataFactory/factories/ingest00-integration-dataFactory001-prod/managedVirtualNetworks/default/managedPrivateEndpoints/ingest00-integration-dataFactory001-keyvault-prod"
+}
