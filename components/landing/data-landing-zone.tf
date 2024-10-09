fe45cbc13b9abb5cca73eb4924ba8c67d08fc2be
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-10-09 17:03:37 +0100

    updated back to main

--- components/landing/data-landing-zone.tf
+++ components/landing/data-landing-zone.tf
@@ -1,5 +1,5 @@
 module "data_landing_zone" {
-  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=DTSPO-18828-master"
+  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=main"
 
   for_each = var.landing_zones
 
