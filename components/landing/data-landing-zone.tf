2d04f073e8f6281ea6c91eb874a55e5025a4be2d
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-10-09 14:40:22 +0100

    using DTSPO-18828-master branch to test the module changes on the terraform-module-data-landing-zone

--- components/landing/data-landing-zone.tf
+++ components/landing/data-landing-zone.tf
@@ -1,5 +1,5 @@
 module "data_landing_zone" {
-  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=main"
+  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=DTSPO-18828-master"
 
   for_each = var.landing_zones
 
