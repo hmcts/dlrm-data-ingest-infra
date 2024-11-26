4ca7aa77d96319d41b197c8d41a9ee94a37f7bd0
Alex Bance <Alex.Bance@justice.gov.uk>
2024-11-26 13:09:42 +0000

    fix: reference to resource groups

--- components/landing/logic.tf
+++ components/landing/logic.tf
@@ -8,7 +8,7 @@ module "logic" {
   }
   env                        = var.env
   landing_zone_key           = each.key
-  resource_group_name        = "ingest${each.key}-logic-${var.env}"
+  resource_group_name        = [for rg in module.data_landing_zone[each.key].resource_groups : rg.name if strcontains(rg.name, "logic")][0]
   common_tags                = merge(module.ctags.common_tags, { "Data-Ingest-Project" = each.value.project })
   private_endpoint_subnet_id = module.data_landing_zone[each.key].subnet_ids["vnet-services"]
   logicapp_subnet_id         = module.data_landing_zone[each.key].subnet_ids["vnet-data-product-001"]
