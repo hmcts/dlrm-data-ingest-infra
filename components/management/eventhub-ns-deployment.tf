f08c6c3986439d1ddac4f30bc2b271221a27b5c0
Jamie McConnell <jamie.mcconnell@justice.gov.uk>
2025-02-19 14:38:18 +0000

    Added ARM template for deployment of existing eventhubs

---
+++ components/management/eventhub-ns-deployment.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_template_deployment" "sbox-dlrm-eventhub-ns" {
+  name                = "sbox-dlrm-eventhub-ns"
+  resource_group_name = "ingest-mgmt-rg-sbox"
+  template_content    = file("modules/ipkit_logic/templates/eventhub_ns_template.json")
+
+  parameters = {
+    eventhubNamespaceName = var.eventhub_namespace_name
+  }
+
+  parameters_body = file("modules/ipkit_logic/templates/parameters.json")
+
+  deployment_mode = "Incremental"
+}
--- components/management/inputs-optional.tf
+++ components/management/inputs-optional.tf
@@ -44,3 +44,4 @@ variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
 }
+
