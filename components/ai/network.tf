a4984376b691fbfc2a29e79367990add1d679528
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-21 13:47:16 +0000

    fix typo

--- components/ai/network.tf
+++ components/ai/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "private_endpoint_subnet" {
   name                 = "ingest00-private-endpoints-${var.env}"
-  resource_group_name  = dat.azurerm_virtual_network.vnet.resource_group_name
+  resource_group_name  = data.azurerm_virtual_network.vnet.resource_group_name
   virtual_network_name = data.azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.247.5.0/27"]
 }
