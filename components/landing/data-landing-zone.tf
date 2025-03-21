bed8b598ec5e68094215af8eb2319e351950bfe7
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-21 11:07:02 +0000

    update branch

--- components/landing/data-landing-zone.tf
+++ components/landing/data-landing-zone.tf
@@ -1,5 +1,5 @@
 module "data_landing_zone" {
-  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=feat%2Fbastion"
+  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=DTSPO-24907/autoo-pause-disabled"
 
   for_each = var.landing_zones
 
