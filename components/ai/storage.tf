0a59cf2d75037d7db7246ee57a77aab4f26efb61
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-21 08:46:43 +0000

    add tags module

--- components/ai/storage.tf
+++ components/ai/storage.tf
@@ -6,4 +6,12 @@ module "storage" {
   location                 = var.location
   account_kind             = "Standard"
   account_replication_type = "ZRS"
+  tags                     = module.common_tags.tags
+}
+
+module "common_tags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
 }
