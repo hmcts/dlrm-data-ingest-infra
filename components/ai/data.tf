a4b6c4f59c24eabe8166d361deec82e8339c9dfd
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-21 13:58:39 +0000

    fix typo

--- components/ai/data.tf
+++ components/ai/data.tf
@@ -2,6 +2,6 @@ data "azurerm_client_config" "current" {
 }
 
 data "azurerm_virtual_network" "vnet" {
-  name                = "ingest00-vnet-{$var.env}"
+  name                = "ingest00-vnet-${var.env}"
   resource_group_name = "ingest00-network-${var.env}"
 }
