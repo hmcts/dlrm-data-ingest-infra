ffd1570a5cd564de3b435c83be0f06f3a2e73681
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-24 11:40:12 +0000

    update gh_runners

--- components/landing/data-landing-zone.tf
+++ components/landing/data-landing-zone.tf
@@ -34,9 +34,7 @@ module "data_landing_zone" {
   additional_subnets = length(each.value.additional_subnets) == 0 ? {} : merge(
     each.value.additional_subnets,
     each.key == "gh_runners" ? {
-      "gh_runners_subnet" = {
-        address_prefixes = [cidrsubnet(cidrsubnet(local.data_ingest_address_space, 6, local.subnet_starting_index[var.env] + (parseint(each.key, 10) * 2)), 3, 6)]
-      }
+      address_prefixes = [cidrsubnet(cidrsubnet(local.data_ingest_address_space, 6, local.subnet_starting_index[var.env] + (parseint(each.key, 10) * 2)), 3, 6)]
     } : {}
   )
 
