b1d4c9dcc9fc79d79a14b8da36a3bb26a6578fed
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-25 09:11:23 +0000

    rename rg

--- components/ai/resource_group.tf
+++ components/ai/resource_group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "dlrm-aria-${var.env}"
+  name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
 
   tags = module.common_tags.common_tags
