0d254d5bdb2bcd10c959dfcfe91d3e500e4be455
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-31 13:56:53 +0100

    update data resource

--- components/ai/data.tf
+++ components/ai/data.tf
@@ -15,6 +15,6 @@ data "azurerm_subnet" "private_endpoint_subnet" {
 data "azurerm_subnet" "azure_devops_agent_subnet" {
   provider             = azurerm.ssptl
   name                 = "aks-00"
-  virtual_network_name = "ss-ptl-vnet"
-  resource_group_name  = "ss-ptl-network-rg"
+  virtual_network_name = var.env == sbox ? "ss-ptlsbox-vnet" : "ss-ptl-vnet"
+  resource_group_name  = var.env == sbox ? "ss-ptlsbox-network-rg" : "ss-ptl-network-rg"
 }
