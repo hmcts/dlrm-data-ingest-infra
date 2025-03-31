b6b42724739c1c42619d9936f9876192bdb92cc2
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-03-31 12:55:51 +0100

    fix reference

--- components/ai/storage.tf
+++ components/ai/storage.tf
@@ -10,7 +10,7 @@ module "storage" {
   private_endpoint_subnet_id    = data.azurerm_subnet.private_endpoint_subnet.id
   public_network_access_enabled = false
   ip_rules                      = var.ip_rules
-  sa_subnets                    = data.azure_devops_agent_subnet.subnet_id
+  sa_subnets                    = data.azure_devops_agent_subnet.id
 }
 
 module "common_tags" {
