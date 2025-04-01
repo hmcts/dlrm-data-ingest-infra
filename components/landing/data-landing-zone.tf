9eb678a33a5322e8c8d7b9d5541c7c55c8115178
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-01 13:53:31 +0100

    use main

--- components/landing/data-landing-zone.tf
+++ components/landing/data-landing-zone.tf
@@ -1,5 +1,5 @@
 module "data_landing_zone" {
-  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=DTSPO-24907/add-service-endpoint"
+  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=main"
 
   for_each = var.landing_zones
 
