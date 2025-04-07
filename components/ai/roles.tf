cfd53c8d362756e6221e65ff0422f440d849298b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-07 14:07:51 +0100

    fix group

--- components/ai/roles.tf
+++ components/ai/roles.tf
@@ -1,15 +1,7 @@
-resource "azuread_group" "poc_group" {
-  display_name     = "POC Group"
-  mail_enabled     = false
-  security_enabled = true
-  owners           = [data.azuread_user.user.object_id]
-  members          = [data.azuread_user.user.object_id]
-}
-
 resource "azurerm_role_assignment" "poc_group" {
 
   scope                = azurerm_resource_group.rg.id
   role_definition_name = "Contributor"
-  principal_id         = azuread_group.poc_group.object_id
+  principal_id         = data.azuread_group.poc_group.object_id
 
 }
