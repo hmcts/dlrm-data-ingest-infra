ec4cbed9d4b1417480575b5fad04ba4d274e5648
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-11 09:42:18 +0100

    use branch

--- components/landing/data-landing-zone.tf
+++ components/landing/data-landing-zone.tf
@@ -1,5 +1,5 @@
 module "data_landing_zone" {
-  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=main"
+  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=revert-21-feature_databricks1_config"
 
   for_each = var.landing_zones
 
