21913506fe8fef047b118085f336e486c9513b90
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-06-05 11:16:21 +0100

    add roles

--- components/ai/roles.tf
+++ components/ai/roles.tf
@@ -5,3 +5,17 @@ resource "azurerm_role_assignment" "poc_group" {
   principal_id         = data.azuread_group.poc_group.object_id
 
 }
+
+resource "azurerm_role_assignment" "blob_contributor_from_compute_instance" {
+  count                = var.env == "prod" ? 1 : 0
+  scope                = data.azurerm_storage_account.ingest_storage_account.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = module.ai.compute_instance_identity
+}
+
+resource "azurerm_role_assignment" "sa_contributor_from_compute_instance" {
+  count                = var.env == "prod" ? 1 : 0
+  scope                = data.azurerm_storage_account.ingest_storage_account.id
+  role_definition_name = "Contributor"
+  principal_id         = module.ai.compute_instance_identity
+}
