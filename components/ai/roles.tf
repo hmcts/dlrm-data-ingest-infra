6f7f8bb7c598944cc4fd4e1675afe02345131530
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-06-05 14:51:57 +0100

    fix typo

--- components/ai/roles.tf
+++ components/ai/roles.tf
@@ -6,7 +6,7 @@ resource "azurerm_role_assignment" "poc_group" {
 
 }
 
-resource "azurerm_role_assignment" "poc_group" {
+resource "azurerm_role_assignment" "poc_group_sa_blob_contributor" {
 
   scope                = data.azurerm_storage_account.ingest_storage_account.id
   role_definition_name = "Storage Account Data Contributor"
@@ -14,7 +14,7 @@ resource "azurerm_role_assignment" "poc_group" {
 
 }
 
-resource "azurerm_role_assignment" "poc_group" {
+resource "azurerm_role_assignment" "poc_group_sa_contributor" {
 
   scope                = data.azurerm_storage_account.ingest_storage_account.id
   role_definition_name = "Contributor"
