7935f4066a73557e058bc35ecde9f9ec98623f8d
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-06-05 13:33:42 +0100

    use main

--- components/ai/ai.tf
+++ components/ai/ai.tf
@@ -1,5 +1,5 @@
 module "ai" {
-  source                       = "git::https://github.com/hmcts/terraform-module-ai-services?ref=DTSPO-26119/use-compute-instance"
+  source                       = "git::https://github.com/hmcts/terraform-module-ai-services?ref=main"
   env                          = var.env
   project                      = var.project
   existing_resource_group_name = azurerm_resource_group.rg.name
