0b85d5e40c2e5f7de13cf85e11ef92aa68ebe56f
Alex Bance <Alex.Bance@justice.gov.uk>
2025-07-17 10:52:18 +0100

    fix: correct branch ref

--- components/landing/data-landing-zone.tf
+++ components/landing/data-landing-zone.tf
@@ -1,5 +1,5 @@
 module "data_landing_zone" {
-  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=feat%2Fsupport-custom-image"
+  source = "github.com/hmcts/terraform-module-data-landing-zone?ref=main"
 
   for_each = var.landing_zones
 
