2c945e160dd7b1ae9ab46bd121f4939e707519b9
Charles.Luo <charles.luo1@justice.gov.uk>
2025-10-14 09:49:13 +0100

    commented out importing peerings hub-to-ingest01-vnet-sbox and hub-to-ingest05-vnet-sbox

--- components/landing/import-peerings.tf
+++ components/landing/import-peerings.tf
@@ -5,13 +5,13 @@ import {
 }
 
 # import hub-to-ingest01-vnet-sbox peering
-import {
-  to = module.data_landing_zone["01"].module.vnet_peer_hub.azurerm_virtual_network_peering.target_to_initiator
-  id = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest01-network-sbox/providers/Microsoft.Network/virtualNetworks/ingest01-vnet-sbox"
-}
+# import {
+#   to = module.data_landing_zone["01"].module.vnet_peer_hub.azurerm_virtual_network_peering.target_to_initiator
+#   id = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest01-network-sbox/providers/Microsoft.Network/virtualNetworks/ingest01-vnet-sbox"
+# }
 
 # import hub-to-ingest05-vnet-sbox peering
-import {
-  to = module.data_landing_zone["05"].module.vnet_peer_hub.azurerm_virtual_network_peering.target_to_initiator
-  id = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest05-network-sbox/providers/Microsoft.Network/virtualNetworks/ingest05-vnet-sbox"
-}
+# import {
+#   to = module.data_landing_zone["05"].module.vnet_peer_hub.azurerm_virtual_network_peering.target_to_initiator
+#   id = "/subscriptions/df72bb30-d6fb-47bd-82ee-5eb87473ddb3/resourceGroups/ingest05-network-sbox/providers/Microsoft.Network/virtualNetworks/ingest05-vnet-sbox"
+# }
