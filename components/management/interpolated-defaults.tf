fc51ac6b84c622f87f6fb7ed52a62c713e422828
Rees Pozzi <rees.pozzi@justice.gov.uk>
2025-10-21 09:31:56 +0100

    sbox peered to nonprod hub
    sbox peered to nonprod hub with stg and prd environments_components commented out
    
    ---------
    
    Co-authored-by: Alex Bance <Alex.Bance@justice.gov.uk>
    Co-authored-by: Charles.Luo <charles.luo1@justice.gov.uk>

--- components/management/interpolated-defaults.tf
+++ components/management/interpolated-defaults.tf
@@ -1,7 +1,7 @@
 locals {
   is_prod            = length(regexall(".*(prod).*", var.env)) > 0
   is_sbox            = length(regexall(".*(s?box).*", var.env)) > 0
-  ssptl_sub_id       = local.is_sbox ? "64b1c6d6-1481-44ad-b620-d8fe26a2c768" : "6c4d2513-a873-41b4-afdd-b05a33206631"
+  ssptl_sub_id       = "6c4d2513-a873-41b4-afdd-b05a33206631"
   cftptl_sub_id      = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
   soc_sub_id         = "8ae5b3b6-0b12-4888-b894-4cec33c92292"
   cnp_prod_sub_id    = "8999dec3-0104-4a27-94ee-6588559729d1"
