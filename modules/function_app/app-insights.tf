e47db36b25cd0237951268858396c64ec49009a6
Ciaran Carragher <ciaranca@kainos.com>
2026-09-02 14:59:00 +0100

    DTSPO-34103: fix function app storage ACL and App Insights name collision
    - Add Microsoft.Storage service endpoint to function-app subnet so it can be used in the storage account network rules
    - Rename function app App Insights product to avoid colliding with the logic module in the shared logic RG

--- modules/function_app/app-insights.tf
+++ modules/function_app/app-insights.tf
@@ -1,7 +1,7 @@
 module "application_insights" {
   source = "github.com/hmcts/terraform-module-application-insights?ref=4.x"
 
-  product             = "data-landing"
+  product             = "data-landing-fa"
   env                 = var.env
   resource_group_name = var.resource_group_name
   common_tags         = var.common_tags
